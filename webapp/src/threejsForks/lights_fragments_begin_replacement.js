f29668d9c64f6eabacaa92348c59fcae4dbe4327
Joe Muoio <joegm258@gmail.com>
2020-10-29 14:25:46 -0700

    Adding in the light fragment fix for certain hardware

---
+++ webapp/src/threejsForks/lights_fragments_begin_replacement.js
@@ -0,0 +1,183 @@
+export default /* glsl */`
+/**
+ * This is a template that can be used to light a material, it uses pluggable
+ * RenderEquations (RE)for specific lighting scenarios.
+ *
+ * Instructions for use:
+ * - Ensure that both RE_Direct, RE_IndirectDiffuse and RE_IndirectSpecular are defined
+ * - If you have defined an RE_IndirectSpecular, you need to also provide a Material_LightProbeLOD. <---- ???
+ * - Create a material parameter that is to be passed as the third parameter to your lighting functions.
+ *
+ * TODO:
+ * - Add area light support.
+ * - Add sphere light support.
+ * - Add diffuse light probe (irradiance cubemap) support.
+ */
+
+GeometricContext geometry;
+
+geometry.position = - vViewPosition;
+geometry.normal = normal;
+geometry.viewDir = ( isOrthographic ) ? vec3( 0, 0, 1 ) : normalize( vViewPosition );
+
+#ifdef CLEARCOAT
+
+	geometry.clearcoatNormal = clearcoatNormal;
+
+#endif
+
+IncidentLight directLight;
+
+#if ( NUM_POINT_LIGHTS > 0 ) && defined( RE_Direct )
+
+	PointLight pointLight;
+	#if defined( USE_SHADOWMAP ) && NUM_POINT_LIGHT_SHADOWS > 0
+	PointLightShadow pointLightShadow;
+	#endif
+
+	#pragma unroll_loop_start
+	for ( int i = 0; i < NUM_POINT_LIGHTS; i ++ ) {
+
+		pointLight.position = pointLights[ i ].position;
+		pointLight.color = pointLights[ i ].color;
+		pointLight.distance = pointLights[ i ].distance;
+		pointLight.decay = pointLights[ i ].decay;
+		pointLight.shadow = pointLights[ i ].shadow;
+		pointLight.shadowBias = pointLights[ i ].shadowBias;
+		pointLight.shadowRadius = pointLights[ i ].shadowRadius;
+		pointLight.shadowMapSize = pointLights[ i ].shadowMapSize;
+		pointLight.shadowCameraNear = pointLights[ i ].shadowCameraNear;
+		pointLight.shadowCameraFar = pointLights[ i ].shadowCameraFar;
+
+		getPointDirectLightIrradiance( pointLight, geometry, directLight );
+
+		#if defined( USE_SHADOWMAP ) && ( UNROLLED_LOOP_INDEX < NUM_POINT_LIGHT_SHADOWS )
+		pointLightShadow = pointLightShadows[ i ];
+		directLight.color *= all( bvec2( directLight.visible, receiveShadow ) ) ? getPointShadow( pointShadowMap[ i ], pointLightShadow.shadowMapSize, pointLightShadow.shadowBias, pointLightShadow.shadowRadius, vPointShadowCoord[ i ], pointLightShadow.shadowCameraNear, pointLightShadow.shadowCameraFar ) : 1.0;
+		#endif
+
+		RE_Direct( directLight, geometry, material, reflectedLight );
+
+	}
+	#pragma unroll_loop_end
+
+#endif
+
+#if ( NUM_SPOT_LIGHTS > 0 ) && defined( RE_Direct )
+
+	SpotLight spotLight;
+	#if defined( USE_SHADOWMAP ) && NUM_SPOT_LIGHT_SHADOWS > 0
+	SpotLightShadow spotLightShadow;
+	#endif
+
+	#pragma unroll_loop_start
+	for ( int i = 0; i < NUM_SPOT_LIGHTS; i ++ ) {
+
+		spotLight.position = spotLights[ i ].position;
+		spotLight.direction = spotLights[ i ].direction;
+		spotLight.color = spotLights[ i ].color;
+		spotLight.distance = spotLights[ i ].distance;
+		spotLight.decay = spotLights[ i ].decay;
+		spotLight.coneCos = spotLights[ i ].coneCos;
+		spotLight.penumbraCos = spotLights[ i ].penumbraCos;
+		spotLight.shadow = spotLights[ i ].shadow;
+		spotLight.shadowBias = spotLights[ i ].shadowBias;
+		spotLight.shadowRadius = spotLights[ i ].shadowRadius;
+		spotLight.shadowMapSize = spotLights[ i ].shadowMapSize;
+
+		getSpotDirectLightIrradiance( spotLight, geometry, directLight );
+
+		#if defined( USE_SHADOWMAP ) && ( UNROLLED_LOOP_INDEX < NUM_SPOT_LIGHT_SHADOWS )
+		spotLightShadow = spotLightShadows[ i ];
+		directLight.color *= all( bvec2( directLight.visible, receiveShadow ) ) ? getShadow( spotShadowMap[ i ], spotLightShadow.shadowMapSize, spotLightShadow.shadowBias, spotLightShadow.shadowRadius, vSpotShadowCoord[ i ] ) : 1.0;
+		#endif
+
+		RE_Direct( directLight, geometry, material, reflectedLight );
+
+	}
+	#pragma unroll_loop_end
+
+#endif
+
+#if ( NUM_DIR_LIGHTS > 0 ) && defined( RE_Direct )
+
+	DirectionalLight directionalLight;
+	#if defined( USE_SHADOWMAP ) && NUM_DIR_LIGHT_SHADOWS > 0
+	DirectionalLightShadow directionalLightShadow;
+	#endif
+
+	#pragma unroll_loop_start
+	for ( int i = 0; i < NUM_DIR_LIGHTS; i ++ ) {
+
+		directionalLight.direction = directionalLights[ i ].direction;
+		directionalLight.color = directionalLights[ i ].color;
+		directionalLight.shadow = directionalLights[ i ].shadow;
+		directionalLight.shadowBias = directionalLights[ i ].shadowBias;
+		directionalLight.shadowRadius = directionalLights[ i ].shadowRadius;
+		directionalLight.shadowMapSize = directionalLights[ i ].shadowMapSize;
+
+		getDirectionalDirectLightIrradiance( directionalLight, geometry, directLight );
+
+		#if defined( USE_SHADOWMAP ) && ( UNROLLED_LOOP_INDEX < NUM_DIR_LIGHT_SHADOWS )
+		directionalLightShadow = directionalLightShadows[ i ];
+		directLight.color *= all( bvec2( directLight.visible, receiveShadow ) ) ? getShadow( directionalShadowMap[ i ], directionalLightShadow.shadowMapSize, directionalLightShadow.shadowBias, directionalLightShadow.shadowRadius, vDirectionalShadowCoord[ i ] ) : 1.0;
+		#endif
+
+		RE_Direct( directLight, geometry, material, reflectedLight );
+
+	}
+	#pragma unroll_loop_end
+
+#endif
+
+#if ( NUM_RECT_AREA_LIGHTS > 0 ) && defined( RE_Direct_RectArea )
+
+	RectAreaLight rectAreaLight;
+
+	#pragma unroll_loop_start
+	for ( int i = 0; i < NUM_RECT_AREA_LIGHTS; i ++ ) {
+
+		rectAreaLight.color = rectAreaLight[ i ].color;
+		rectAreaLight.position = rectAreaLight[ i ].position;
+		rectAreaLight.halfWidth = rectAreaLight[ i ].halfWidth;
+        rectAreaLight.halfHeight = rectAreaLight[ i ].halfHeight;
+        
+		RE_Direct_RectArea( rectAreaLight, geometry, material, reflectedLight );
+
+	}
+	#pragma unroll_loop_end
+
+#endif
+
+#if defined( RE_IndirectDiffuse )
+
+	vec3 iblIrradiance = vec3( 0.0 );
+
+	vec3 irradiance = getAmbientLightIrradiance( ambientLightColor );
+
+	irradiance += getLightProbeIrradiance( lightProbe, geometry );
+
+	#if ( NUM_HEMI_LIGHTS > 0 )
+
+		#pragma unroll_loop_start
+		for ( int i = 0; i < NUM_HEMI_LIGHTS; i ++ ) {
+
+			hemisphereLight.direction = hemisphereLights[ i ].direction;
+			hemisphereLight.skyColor = hemisphereLights[ i ].skyColor;
+			hemisphereLight.groundColor = hemisphereLights[ i ].groundColor;
+			irradiance += getHemisphereLightIrradiance( hemisphereLight, geometry );
+
+		}
+		#pragma unroll_loop_end
+
+	#endif
+
+#endif
+
+#if defined( RE_IndirectSpecular )
+
+	vec3 radiance = vec3( 0.0 );
+	vec3 clearcoatRadiance = vec3( 0.0 );
+
+#endif
+`;
